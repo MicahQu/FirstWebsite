a0f9e8b2bdfa097e9c2d5026b08be807495e5f60
MicahQu <142445353+MicahQu@users.noreply.github.com>
2024-09-04 10:03:52 -0500

    think I am pushing this too the right branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SearchPageComponent } from './search-page/search-page.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchPageComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
